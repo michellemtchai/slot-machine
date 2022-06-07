9f96da1afecb6713d51c3e756a6dbf44a75d014e
michellemtchai <michelle.chai@mail.utoronto.ca>
2022-06-06 23:15:36 -0400

    add postinstall script

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
-release: chmod u+x ./docker/heroku.sh && ./docker/heroku.sh
 web: APP_PORT="$PORT" yarn prod
--- docker/heroku.sh
+++ docker/heroku.sh
@@ -11,8 +11,5 @@ yarn build
 cd ..
 
 # copy angular build files
-echo "moving files to /public"
 mv -v ./frontend/build/* ./public
-echo "finish moving files"
 node ./docker/build-frontend.js
-echo "finish generating frontend"
